02bcdc0dc35bff8234d235c471c81afa99b7c803
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-01-03 14:27:25 +0300

    move interactive cli in package

---
+++ src/cli/main.py
@@ -0,0 +1,26 @@
+from cmd import Cmd
+
+
+class DeployerPrompt(Cmd):
+    def do_q(self, args):
+        print("Quitting.")
+        raise SystemExit
+
+    def emptyline(self):
+        pass
+
+
+def main():
+    prompt = DeployerPrompt()
+    prompt.prompt = 'deployer > '
+    prompt.doc_leader = 'Deployer commands'
+    prompt.doc_header = 'list commands'
+    prompt.intro = 'Interactive deployer\n for deploy on production'
+
+    projectPrompt = ''
+
+    prompt.cmdloop(
+        'Welcome to deployer...\n'
+        + 'Other:\n'
+        + '\t [ q ] quite'
+    )
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from cmd import Cmd
+import cli.main as main_cli
 from server import main
 import sys
 
@@ -33,26 +33,4 @@ else:
         print(helpText)
         raise SystemExit
 
-
-class DeployerPrompt(Cmd):
-    def do_q(self, args):
-        print("Quitting.")
-        raise SystemExit
-
-    def emptyline(self):
-        pass
-
-
-prompt = DeployerPrompt()
-prompt.prompt = 'deployer > '
-prompt.doc_leader = 'Deployer commands'
-prompt.doc_header = 'list commands'
-prompt.intro = 'Interactive deployer\n for deploy on production'
-
-projectPrompt = ''
-
-prompt.cmdloop(
-    'Welcome to deployer...\n'
-    + 'Other:\n'
-    + '\t [ q ] quite'
-)
+main_cli.main()
