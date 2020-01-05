8533562284594218deac1faddfbaf0851ae01a36
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-01-05 11:26:21 +0300

    add step

--- src/project/build.py
+++ src/project/build.py
@@ -1,7 +1,15 @@
+import project.step as step
+
+
 class Build:
     name: str
     description: str
+    steps: list
 
     def __init__(self, name: str, description: str):
         self.name = name
         self.description = description
+        self.steps = []
+
+    def add_step(self, step: step.Step):
+        self.steps.append(step)
---
+++ src/project/step.py
@@ -0,0 +1,5 @@
+class Step:
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
