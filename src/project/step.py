6ed228909f23f635be567765e4a92bf79ae5e119
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-01-07 13:31:12 +0300

    extend validation for step

--- src/project/step.py
+++ src/project/step.py
@@ -1,13 +1,36 @@
+allowed_actions = [
+    'run-custom-shell'
+]
+
+
 class Step:
     name: str
     description: str
+    action: str
+    command: str
 
-    def __init__(self, name: str, description: str):
+    def __init__(self, name: str, description: str, action: str):
         self.name = name
         self.description = description
+        self.action = action
+
+    def set_command(self, command: str):
+        self.command = command
 
 
 def parse(data: dict) -> Step:
     assert 'name' in data.keys(), "Expected field 'name' exists"
     assert 'description' in data.keys(), "Expected field 'description' exists"
-    return Step(data.get('name'), data.get('description'))
+    assert 'action' in data.keys(), "Expected field 'action' exists"
+    action = data.get('action')
+    if action not in allowed_actions:
+        raise AssertionError
+
+    step = Step(data.get('name'), data.get('description'), action)
+
+    if action == 'run-custom-shell':
+        assert 'command' in data.keys(), "Expected field 'command' exists"
+        step.set_command(data.get('command'))
+
+    return step
+
