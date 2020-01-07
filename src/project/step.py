bf8f608e4ed91d2ed0168849c71d2272462ce45f
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-01-07 13:22:14 +0300

    extend steps

--- src/project/step.py
+++ src/project/step.py
@@ -1,10 +1,13 @@
 class Step:
     name: str
+    description: str
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, description: str):
         self.name = name
+        self.description = description
 
 
 def parse(data: dict) -> Step:
     assert 'name' in data.keys(), "Expected field 'name' exists"
-    return Step(data.get('name'))
+    assert 'description' in data.keys(), "Expected field 'description' exists"
+    return Step(data.get('name'), data.get('description'))
