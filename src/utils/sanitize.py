e6abaa802f23ff3ce0321f92c18e85476c45c03e
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-02-02 12:52:18 +0300

    translit

--- src/utils/sanitize.py
+++ src/utils/sanitize.py
@@ -1,7 +1,52 @@
+symbol_map = {
+    'а': 'a',
+    'б': 'b',
+    'в': 'v',
+    'г': 'g',
+    'д': 'd',
+    'е': 'e',
+    'ё': 'e',
+    'ж': 'zh',
+    'з': 'z',
+    'и': 'i',
+    'й': 'y',
+    'к': 'k',
+    'л': 'l',
+    'м': 'm',
+    'н': 'n',
+    'о': 'o',
+    'п': 'p',
+    'р': 'r',
+    'с': 's',
+    'т': 't',
+    'у': 'u',
+    'ф': 'f',
+    'х': 'h',
+    'ц': 'c',
+    'ч': 'ch',
+    'ш': 'sh',
+    'щ': 'sch',
+    'ъ': '',
+    'ы': 'i',
+    'ь': '',
+    'э': 'e',
+    'ю': 'yu',
+    'я': 'ya',
+}
+
+
 def sanitize(data: str) -> str:
     result = ''
     for char in data:
         if 'a' <= char <= 'z' or 'A' <= char <= 'Z' or '0' <= char <= '9':
             result += char
+        else:
+            if char.islower():
+                if char in symbol_map.keys():
+                    result += symbol_map.get(char)
+
+            if char.isupper():
+                if char.lower() in symbol_map.keys():
+                    result += symbol_map.get(char.lower()).upper()
 
     return result
--- tests/utils/sanitize_test.py
+++ tests/utils/sanitize_test.py
@@ -6,8 +6,17 @@ class TestSanitizeMethod(unittest.TestCase):
     def testSanitize_LatinLetters_ExpectedResult(self):
         self.assertEqual('result', sanitize.sanitize('result'))
 
+    def testSanitize_LatinCapitalizedLetters_ExpectedResult(self):
+        self.assertEqual('RESULT', sanitize.sanitize('RESULT'))
+
     def testSanitize_LatinLettersWithDigits_ExpectedResult(self):
         self.assertEqual('result10', sanitize.sanitize('result10'))
+
+    def testSanitize_CyrillicLetters_ExpectedResult(self):
+        self.assertEqual('rezultat', sanitize.sanitize('результат'))
+
+    def testSanitize_CapitalizedCyrillicLetters_ExpectedResult(self):
+        self.assertEqual('REZULTAT', sanitize.sanitize('РЕЗУЛЬТАТ'))
 
 
 if __name__ == '__main__':
