7fe0fd1f79c92b54207cfac8a2b9d36861d85778
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-02-15 12:40:39 +0300

    add test cases

--- tests/utils/digit_to_letter_test.py
+++ tests/utils/digit_to_letter_test.py
@@ -3,8 +3,14 @@ import src.utils.digit_to_letter as digit_to_letter
 
 
 class TestSanitizeMethod(unittest.TestCase):
-    def testConvert_SomeDigit_ExpectedResult(self):
+    def testConvert_SomeDigit01_ExpectedResult(self):
         self.assertEqual('u0Cd', digit_to_letter.digit_to_letter(7866765))
+
+    def testConvert_SomeDigit02_ExpectedResult(self):
+        self.assertEqual('1uhXMZ', digit_to_letter.digit_to_letter(1581759549))
+
+    def testConvert_SomeDigit03_ExpectedResult(self):
+        self.assertEqual('zZMRCIOA', digit_to_letter.digit_to_letter(158175954914468))
 
 
 if __name__ == '__main__':
